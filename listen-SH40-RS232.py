3f74502cc215aba480d2950b4802f41ece3362ac
Leopold Weidner <81673643+leopold-weidner@users.noreply.github.com>
2025-01-10 20:49:29 +0100

    main script created

---
+++ listen-SH40-RS232.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 10 20:47:03 2025
+
+@author: lw
+
+This is the main script which will execute a query loop to periodically get SH40 actuals.
+Potential upload to an influxd instance following later.
+
+"""
